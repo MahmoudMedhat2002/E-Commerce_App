31b93e389f9b92247efda0694a70f9352dc661ae
MahmoudMedhat2002 <dodomado2023@gmail.com>
2023-12-06 23:43:18 +0200

    Created First Model

---
+++ E-Commerce_App/Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿namespace E_Commerce_App.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+
+        public string ProfilePictureURL { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
