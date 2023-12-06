f688a8c26c6c3a9e1d07f9b6575d08c06c1e0cf7
MahmoudMedhat2002 <dodomado2023@gmail.com>
2023-12-06 23:21:04 +0200

    Add project files.

---
+++ E-Commerce_App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Commerce_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
