d44a85c292871ec9191f352f05a0aaf3f51d5651
MahmoudMedhat2002 <dodomado2023@gmail.com>
2023-12-07 16:02:25 +0200

    ControllesCreated

---
+++ E-Commerce_App/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_App.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public MoviesController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
