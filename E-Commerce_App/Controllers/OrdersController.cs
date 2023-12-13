5e365da3a624a5e2e5b8ce2887fef8422168c876
MahmoudMedhat2002 <dodomado2023@gmail.com>
2023-12-13 22:46:57 +0200

    Add Shopping Cart View

---
+++ E-Commerce_App/Controllers/OrdersController.cs
@@ -0,0 +1,36 @@
+﻿using E_Commerce_App.Data.Cart;
+using E_Commerce_App.Repository;
+using E_Commerce_App.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace E_Commerce_App.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMovieRepository MovieRepository;
+
+        private readonly ShoppingCart ShoppingCart;
+
+        public OrdersController(IMovieRepository MovieRepository , ShoppingCart ShoppingCart)
+        {
+            this.ShoppingCart = ShoppingCart;
+            this.MovieRepository = MovieRepository;
+        }
+        public IActionResult Index()
+        {
+            var items = ShoppingCart.GetShoppingCartItems();
+            ShoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = ShoppingCart,
+                Total = ShoppingCart.GetTotal()
+            };
+
+
+
+            return View(response);
+        }
+    }
+}
---
+++ E-Commerce_App/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce_App.Data.Cart;
+
+namespace E_Commerce_App.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+
+        public double Total { get; set; }
+    }
+}
